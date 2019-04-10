b69f44003c316bcc537abf37c2e1fe2090cf77f9
Михаил <borovkovm177@gmail.com>
2019-04-10 21:37:37 +0300

    валидаторы

--- Server/Server/Startup.cs
+++ Server/Server/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.EntityFrameworkCore;
 using Server.DataAccess;
 using Server.Contracts;
 using Server.Service;
+using FluentValidation.AspNetCore;
 
 namespace Server
 {
@@ -30,7 +31,9 @@ namespace Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(options => options.UseSqlServer(@"Server=USER-PC;Database=Selection;Trusted_Connection=True;"));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddFluentValidation(fvc =>
+                fvc.RegisterValidatorsFromAssemblyContaining<Startup>());
             services.AddScoped<IDataContext, DataContext>();
             services.AddScoped<IEmployeeService,EmployeeService>();
             services.AddScoped<IVacancyService, VacancySecrvice>();
---
+++ Server/Server/Validators/LastWorkValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using Server.Data;
+
+namespace Server.Validators
+{
+    public class LastWorkValidator : AbstractValidator<DbLastWork>
+    {
+        public LastWorkValidator()
+        {
+            RuleFor(reg => reg.Employee).NotEmpty();
+        }
+    }
+}
