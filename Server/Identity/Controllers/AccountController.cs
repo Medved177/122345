2431bd1b3adcb29608d677db64204d5da481fe5f
Михаил <borovkovm177@gmail.com>
2019-04-16 12:01:55 +0300

    Попытка

---
+++ Server/Identity/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Identity.Models;
+
+namespace Identity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : Controller 
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(ModelUser model)
+        {
+                User user = new User { Email = model.Email, UserName = model.Login };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return Ok();
+                }
+                else  return BadRequest();
+            }
+        }
+    }
---
+++ Server/Identity/Validators/ModelValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using Identity.Models;
+
+namespace Identity.Validators
+{
+    public class ModelValidator : AbstractValidator<ModelUser>
+    {
+        public ModelValidator()
+        {
+            RuleFor(reg=> reg.Login).NotEmpty();
+            RuleFor(reg => reg.Password).NotEmpty();
+            RuleFor(reg => reg.PasswordConfirm).NotEmpty();
+            RuleFor(reg => reg.Email).NotEmpty().EmailAddress();
+        }
+    }
+}
